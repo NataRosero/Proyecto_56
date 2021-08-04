37b867cc6caa36eaff1da96aa19262ad927b5c05
NataRosero <natalia.roserom@gmail.com>
2021-08-03 19:16:38 -0500

    reto 2 conexion de java y sqlite 1 paso

--- tienda_de_ropa/src/App.java
+++ tienda_de_ropa/src/App.java
@@ -1,6 +1,10 @@
+import modelo.*;//el asterisco te importa todas as clases dentro de la carpeta
+import Conexion.SqliteConnection;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("La Tienda");
+
+        SqliteConnection connexionBD = new SqliteConnection();
 
         Producto productoUno = new Producto();
     
